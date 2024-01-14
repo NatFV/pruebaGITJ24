c445bce826c7905f9d099151c4d0fc0eaa47ad62
Usuario <Usuario@DESKTOP-0DILOR8>
2024-01-14 19:26:31 +0100

    7 y media push

--- PracticoGit/src/main/java/principal/prueba.java
+++ PracticoGit/src/main/java/principal/prueba.java
@@ -6,6 +6,7 @@ public class prueba {
 		// TODO Auto-generated method stub
 		System.out.println ("Hola qué tal");
 		System.out.println ("Bien gracias");
+		System.out.println ("Bien ¿y tú?")
 	}
 
 }
