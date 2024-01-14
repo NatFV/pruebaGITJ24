872fc80ae50c69e2ef15fbd9ab2b08e4527bb0f4
NatFV <151751455+NatFV@users.noreply.github.com>
2024-01-14 19:28:04 +0100

    Update prueba.java
    Antes de las 8

--- PracticoGit/src/main/java/principal/prueba.java
+++ PracticoGit/src/main/java/principal/prueba.java
@@ -6,7 +6,8 @@ public class prueba {
 		// TODO Auto-generated method stub
 		System.out.println ("Hola qué tal");
 		System.out.println ("Bien gracias");
-		System.out.println ("Bien ¿y tú?")
+		System.out.println ("Bien ¿y tú?");
+		System.out.println ("Genial, muchas gracias);
 	}
 
 }
